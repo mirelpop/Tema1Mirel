e5e55d63ab7c24a03441b4f9919e52957a654bcd
BojteTamas <bojtetomi92@gmail.com>
2019-10-21 19:33:41 +0300

    Troll

--- tema1/src/Tema1.java
+++ tema1/src/Tema1.java
@@ -1,5 +1,20 @@
 public class Tema1 {
     public static void main(String[] args) {
-        System.out.println("Hello from the other side");
+        System.out.println("sadlkjsamjdnaslk ");
+    }
+    public static void main(String[] args) {
+        System.out.println("sadlkjsamjdnaslk ");
+    }
+}
+public class Tema1 {
+    public static void main(String[] args) {
+    public static void main(String[] args) {
+        System.out.println("sadlkjsamjdnaslk ");
+    }
+}
+public class Tema1 {
+    public static void main(String[] args) {
+        System.out.println("sadlkjsamjdnaslk ");
+    }
     }
 }
